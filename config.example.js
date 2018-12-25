759e307ec6599c3b06c25a734268f87b586193c5
Nimo Chu <nimo.jser@gmail.com>
2018-12-25 16:59:24 +0800

    商户支付

---
+++ config.example.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // https://pay.weixin.qq.com/index.php/core/cert/api_cert API密钥
+    // 微信商户平台>账户中心>安全中心>api安全>api密钥>设置密钥
+    api_key: '',
+    // https://pay.weixin.qq.com/index.php/extend/pay_setting/ma APPID绑定授权申请
+    // 微信商户平台>产品中心>APPID绑定授权申请
+    // 去绑定另外一个微信公众号id
+    mch_appid: '',
+    // https://pay.weixin.qq.com/index.php/extend/employee
+    // 微信商户平台>账户中心>安全中心>个人设置>登录账号
+    mchid: '',
+    // 可以在微信商户平台设置最小金额
+    // 产品中心>点击企业付款到零钱>产品设置
+    // https://pay.weixin.qq.com/index.php/xphp/csptrans_pocket/sptrans_pocket#/pages/product_setting/product_setting
+
+    // 如何获取自己的微信用户openid
+    // https://www.zhihu.com/question/28101897
+    openid: ''
+}
---
+++ mmpaymkttransfers_promotion_transfers.js
@@ -0,0 +1,70 @@
+// https://github.com/onface/wechat-pay-demo
+var request = require('request')
+var path = require('path')
+var fs = require('fs')
+const { toXML } = require('jstoxml')
+var md5 = require('md5')
+var config = require('./config.example.js')
+
+if (fs.existsSync('./config.js')) {
+    config = require('./config.js')
+}
+// 企业付款 https://pay.weixin.qq.com/wiki/doc/api/tools/mch_pay.php?chapter=14_2
+var data = {
+    mch_appid: config.mch_appid,
+    mchid: config.mchid,
+    nonce_str: `${new Date().getTime()*parseInt(Math.random()*100)}`,
+    partner_trade_no: md5(`${Math.random()}`),
+    openid: config.openid,
+    amount: 1,
+    check_name: 'NO_CHECK',
+    spbill_create_ip: '192.168.0.1',
+    desc: '测试发钱'
+}
+// 签名算法 https://pay.weixin.qq.com/wiki/doc/api/tools/mch_pay.php?chapter=4_3
+var sortKey = []
+for(key in data) {
+    sortKey.push(key)
+}
+sortKey = sortKey.sort(function (a, b) {
+    return a.charCodeAt() - b.charCodeAt()
+})
+let queryString = []
+sortKey.forEach(function (key) {
+    queryString.push(`${key}=${data[key]}`)
+})
+
+queryString = queryString.join('&') + `&key=` + config.api_key
+var sign = md5(queryString).toUpperCase()
+data.sign = sign
+
+console.log('requestData', data)
+
+var body = `<xml>${toXML(data)}</xml>`
+// 账户中心>安全中心>API安全>下载证书
+var certFile = path.resolve(__dirname, 'cert/apiclient_cert.pem')
+var keyFile = path.resolve(__dirname, 'cert/apiclient_key.pem')
+
+if (!fs.existsSync(certFile)) {
+    throw new Error('你需要下载安全证书 账户中心>安全中心>API安全>下载证书 https://pay.weixin.qq.com/index.php/core/cert/api_cert')
+}
+if (!fs.existsSync(keyFile)) {
+    throw new Error('你需要下载安全证书  账户中心>安全中心>API安全>下载证书 https://pay.weixin.qq.com/index.php/core/cert/api_cert')
+}
+request({
+    uri: 'https://api.mch.weixin.qq.com/mmpaymkttransfers/promotion/transfers',
+    agentOptions: {
+        cert: fs.readFileSync(certFile),
+        key: fs.readFileSync(keyFile),
+    },
+    method: 'POST',
+    body: body,
+    headers: {
+        'Content-Type': 'application/xml'
+    }
+},
+function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+})
